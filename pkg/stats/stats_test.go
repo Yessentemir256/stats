6a4c5081b8581c614388401f1c95819449fdd658
Yessentemir <yessentemir.utepov@gmail.com>
2024-08-31 09:43:24 +0500

    added additional test for TestTotalInCategory2

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -121,3 +121,21 @@ func TestCategoriesAvg(t *testing.T) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 }
+
+func TestTotalInCategory2(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto", Amount: 1_000_00},
+		{ID: 2, Category: "food", Amount: 2_000_00},
+		{ID: 3, Category: "auto", Amount: 3_000_00},
+		{ID: 4, Category: "auto", Amount: 4_000_00},
+		{ID: 5, Category: "fun", Amount: 5_000_00},
+	}
+
+	expected := types.Money(800000)
+
+	result := TotalInCategory(payments, "auto")
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
